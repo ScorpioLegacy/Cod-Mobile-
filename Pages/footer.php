fcb918b7b63232a5102b976ddd3b39417ff59188
cellum95 <101460624+cellum95@users.noreply.github.com>
2022-03-30 21:12:45 +0800

    Create footer.php

---
+++ Pages/footer.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+
+<?php
+	$gunname;
+?>
+
+<html>
+
+	<head>
+		<meta charset = "utf-8">
+    	<meta http-equiv = "X-UA-Compatible" content = "IE=edge">
+    	<title>COD Gun Guide</title>
+    	<meta name = "description" content = "The main page of the COD Gun Guide website">
+		<link href = "../CSS/style.php" rel = "stylesheet" type = "text/css">
+
+		<style>
+
+			h4 {
+				color:  white;
+				margin-left:  40px;
+			}
+
+		</style>
+
+	</head>
+
+	<body>
+
+		<h4>Hex Clan Copyright &#169 2022</h4>
+
+	</body>
+
+</html>
